d7a63634dde8210c4175ea0712cd46df14af9c8f
alexander <alexanderelton2887@gmail.com>
2024-09-30 22:17:21 -0500

    ad newss

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,10 +103,7 @@
  }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  
    try {
      await client.sql`BEGIN`;
      await seedUsers();
