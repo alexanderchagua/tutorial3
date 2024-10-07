6b22c1c52cab97d5bf89d31e203484eda91ed54d
alexander <alexanderelton2887@gmail.com>
2024-10-07 00:21:04 -0500

    add news

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
+// app/ui/fonts.ts
 import { Inter, Lusitana } from 'next/font/google';
- 
+
+// Asegúrate de que los subsets sean válidos para las fuentes que estás importando
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
-  });
+});
